dc3d81eb781775590cc5c13c48cd649eb6366b43
Caleb Kang <hoyoul@gmail.com>
2019-07-09 14:52:49 -0600

    Added support for spaced in the resourcePath and default to process the output as a string

--- index.js
+++ index.js
@@ -10,20 +10,31 @@ const convertAsync = (command, cb) => childProcess.exec(
 );
 
 const getCellsOnly = (html) => cheerio.load(html)('#notebook-container').html();
-const postProcess = (args, output) => args.cellsOnly ? getCellsOnly(output) : output;
+const postProcess = (args, output) => {
+  const source = args.cellsOnly ? getCellsOnly(output) : output;
+
+  if (args.asString) {
+    const json = JSON.stringify(source)
+      .replace(/\u2028/g, '\\u2028')
+      .replace(/\u2029/g, '\\u2029');
+    return `export default ${json};`;
+  }
+
+  return source;
+};
 
 module.exports = function(content) {
   this.cacheable && this.cacheable(true);
 
-  const args = Object.assign(
-    {
-      cellsOnly: false,
-      to: 'html'
-    },
-    querystring.parse(this.query.substring(1))
-  );
+  const override = querystring.parse(this.query.substring(1));
+  override.cellsOnly = override.cellsOnly === 'true';
+  const args = Object.assign({
+    cellsOnly: false,
+    asString: true,
+    to: 'html'
+  }, override);
 
-  const command = `jupyter nbconvert --to ${args.to} ${this.resourcePath} --stdout`;
+  const command = `jupyter nbconvert --to ${args.to} "${this.resourcePath}" --stdout`;
   const callback = this.async();
 
   if (!callback) return postProcess(args, convertSync(command));
